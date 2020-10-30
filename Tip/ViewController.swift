4c69b73db03fbb16ebc0d4eb2754068433487c1c
Michelle Cheng <michellecheng.2017@gmail.com>
2020-10-30 00:33:55 -0400

    bill amount is always the first responder

--- Tip/ViewController.swift
+++ Tip/ViewController.swift
@@ -23,6 +23,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        billField.becomeFirstResponder()
     }
 
 
