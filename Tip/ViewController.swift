8771d17721bab19de6608435da8887e3ec0a0a7f
Michelle Cheng <michellecheng.2017@gmail.com>
2020-10-29 20:19:36 -0400

    Implemented basic dark mode

--- Tip/ViewController.swift
+++ Tip/ViewController.swift
@@ -9,10 +9,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var billAmountLabel: UILabel!
+    @IBOutlet weak var tipAmountLabel: UILabel!
+    @IBOutlet weak var totalCostLabel: UILabel!
     @IBOutlet weak var billField: UITextField!
     @IBOutlet weak var tipLabel: UILabel!
     @IBOutlet weak var totalLabel: UILabel!
     @IBOutlet weak var tipControl: UISegmentedControl!
+    @IBOutlet weak var darkLabel: UILabel!
+    @IBOutlet weak var outletSwitch: UISwitch!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -39,6 +44,37 @@ class ViewController: UIViewController {
         //Update the tip and total labels
         tipLabel.text = String(format:"$%.2f", tip)
         totalLabel.text = String(format:"$%.2f", total)
+    }
+    
+    
+    @IBAction func darkAction(_ sender: Any) {
+        if outletSwitch.isOn == true {
+            view.backgroundColor = UIColor.black
+            darkLabel.textColor = UIColor.white
+            tipLabel.textColor = UIColor.white
+            totalLabel.textColor = UIColor.white
+            billField.textColor = UIColor.white
+            billAmountLabel.textColor = UIColor.white
+            tipAmountLabel.textColor = UIColor.white
+            totalCostLabel.textColor = UIColor.white
+            tipControl.backgroundColor =  UIColor.white
+            billField.layer.borderColor = UIColor.white.cgColor
+            billField.layer.borderWidth = 0.3
+            
+        }
+        else {
+            view.backgroundColor = UIColor.white
+            darkLabel.textColor =  UIColor.black
+            tipLabel.textColor = UIColor.black
+            totalLabel.textColor = UIColor.black
+            billField.textColor = UIColor.black
+            billAmountLabel.textColor = UIColor.black
+            tipAmountLabel.textColor = UIColor.black
+            totalCostLabel.textColor = UIColor.black
+            tipControl.backgroundColor =  UIColor.white
+            billField.layer.borderColor = UIColor.black.cgColor
+            billField.layer.borderWidth = 0.3
+        }
     }
     
 }
